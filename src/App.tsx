723b81c9c75fa02b7ef71ebc78d1a386ac2ce4e8
Mustapha Stephen <63019472+MagicScripta@users.noreply.github.com>
2023-05-15 04:12:07 -0230

    enable analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
 import { isDesktop, isTablet } from 'react-device-detect'
 import Desktop from './Desktop'
 import Mobile from './Mobile'
+import { inject } from '@vercel/analytics'
 
+inject()
 const App = () => (isDesktop || isTablet ? <Desktop /> : <Mobile />)
 export default App
